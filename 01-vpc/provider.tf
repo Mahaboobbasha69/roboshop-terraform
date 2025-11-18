c00ac0d51193f9f76c1cb36c6a2f458a9ec211c0
myusername <myemail>
2025-11-18 23:54:50 +0530

    Commiting terraform

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "daws76s-remote-state"
+    bucket = "mahaboob-remote-state"
     key    = "vpc"
     region = "us-east-1"
     dynamodb_table = "daws76s-locking"
